f51ce7312c86a0db603fe1fc384dc6b07901d28a
Prajval Chauhan <prajval.chauhan3@gmail.com>
2020-10-09 23:05:29 +0530

    Contents of a contact entry created

---
+++ AddressBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook_final
+{
+    class AddressBook
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string MobileNumber { get; set; }
+        public string eMail { get; set; }
+        public string address { get; set; }
+        public string zipCode { get; set; }
+        public string state { get; set; }
+    }
+}
