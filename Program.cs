52b52834c02a646f24782501840d960723334b0e
Prajval Chauhan <prajval.chauhan3@gmail.com>
2020-10-09 22:58:06 +0530

    Welcome message displayed

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AddressBook_final
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("*** Welcome to the Address Book Program ***\n");
+        }
+    }
+}
