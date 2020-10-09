b071a13aea8de0a1f4197ecb2727c205105d2cf4
Prajval Chauhan <prajval.chauhan3@gmail.com>
2020-10-09 23:55:04 +0530

    can add and save contact now

--- AddressBook.cs
+++ AddressBook.cs
@@ -6,6 +6,7 @@ namespace AddressBook_final
 {
     class AddressBook
     {
+        public static List<AddressBook> Records = new List<AddressBook>();
         public string firstName { get; set; }
         public string lastName { get; set; }
         public string MobileNumber { get; set; }
@@ -13,5 +14,30 @@ namespace AddressBook_final
         public string address { get; set; }
         public string zipCode { get; set; }
         public string state { get; set; }
+
+        public void AddContact()
+        {
+            AddressBook contact = new AddressBook();
+            Console.WriteLine("Enter the first name of the person: ");
+            contact.firstName = Console.ReadLine();
+            Console.WriteLine("Enter the last name of the person: ");
+            contact.lastName = Console.ReadLine();
+            Console.WriteLine("Enter the mobile number of the person: ");
+            contact.MobileNumber = Console.ReadLine();
+            Console.WriteLine("Enter the email ID of the person: ");
+            contact.eMail = Console.ReadLine();
+            Console.WriteLine("Enter the address of the person");
+            contact.address = Console.ReadLine();
+            Console.WriteLine("Enter the ZIP code: ");
+            contact.zipCode = Console.ReadLine();
+            Console.WriteLine("Enter the state: ");
+            contact.state = Console.ReadLine();
+
+            Records.Add(contact);
+        }
+
+
+
+        
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,6 +7,9 @@ namespace AddressBook_final
         static void Main(string[] args)
         {
             Console.WriteLine("*** Welcome to the Address Book Program ***\n");
+            Console.WriteLine("Enter 1 to Add Contact\nEnter 2 to display contacts\nEnter 3 to exit\n");
+            AddressBook call = new AddressBook();
+            call.AddContact();
         }
     }
 }
